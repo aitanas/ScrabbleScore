aa8d4d4e9e392f0b6cfc6595f22a32fe9658a036
Aitana Shough <aeshough@gmail.com>
2022-12-07 09:26:15 -0800

    add passing test code for test 2, update test 1 to include argument

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -10,16 +10,21 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstancesOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
+      //Arrange
       string description = "Walk the dog.";
       Item newItem = new Item(description);
+
+      //Act
       string result = newItem.Description;
+
+      //Assert
       Assert.AreEqual(description, result);
     }
   }
