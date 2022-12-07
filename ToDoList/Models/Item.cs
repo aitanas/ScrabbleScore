192807c32d90a4c75ceacd2c73299b5bcb872d7c
Aitana Shough <aeshough@gmail.com>
2022-12-07 09:55:28 -0800

    add teardown method and verify passing tests

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,26 @@
+using System;
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
 
-    // properties, methods, etc. will go here.
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList 
