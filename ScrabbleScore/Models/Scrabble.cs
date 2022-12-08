531b12f1000bf778e1c72ffe5d81479cc4e26c33
Aitana Shough <aeshough@gmail.com>
2022-12-08 09:46:38 -0800

    add template and boilerplate code
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>

---
+++ ScrabbleScore/Models/Scrabble.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace ScrabbleScore.Models
+{
+  public class Scrabble
+  {
+    
+  }
+}
---
+++ ScrabbleScore/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using ScrabbleScore.Models;
+
+namespace ScrabbleScore
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+      
+    }
+  }
+}
